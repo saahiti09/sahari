3dd51ae332df36df5e9ecaa3b82f5cd881ad06b0
Saahiti <Saahiti09@gmail.com>
2018-01-02 21:10:16 +0000

    added comment line

--- helloworld.java
+++ helloworld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
       System.out.println("welcome to the project");
+//prints the statement
       System.out.println("welcome to the GIT");
    }
 }
